46165e4164e17a3edc191efbd8bfd61fc68a1929
RiadOliveira <riad.oliveira@hotmail.com>
2022-11-20 02:45:14 -0300

    Brutal force algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        ProcessingProvider.KMP("AGGCTTTTTGGTACCACAAACGTAGCGAGCATGAACTTTTCCTAAATTAAGACATTTACGACCTG", "AT");
+        ProcessingProvider.KMP("AAAAABAAABA", "AAAA");
     }
 }
--- src/ProcessingProvider.java
+++ src/ProcessingProvider.java
@@ -2,28 +2,62 @@ public class ProcessingProvider {
     public static void brutalForce(
         String stringToProcess, String pattern
     ) {
+        int patternsFound = 0, comparsionsQuantity = 1;
+        int patternLength = pattern.length();
+
+        for(
+            int stringIndex=0 ;
+            stillNeedsToComparePattern(stringToProcess, stringIndex, pattern) ;
+            stringIndex++
+        ) {
+            for(
+                int paternIndex=0 ;
+                paternIndex < patternLength
+                ; paternIndex++, comparsionsQuantity++
+            ) {
+                char stringChar = stringToProcess.charAt(stringIndex + paternIndex);
+                char patternChar = pattern.charAt(paternIndex);
+
+                if(stringChar != patternChar) break;
+                if(paternIndex != patternLength - 1) continue;
+                patternsFound++;
+
+                System.out.println(
+                    "Foram feitas " + comparsionsQuantity +
+                    " comparações para encontrar o " + patternsFound +
+                    "º padrão."
+                );
+                comparsionsQuantity = 0;
+            }
+        }
+
+        System.out.println("Quantidade de padrões encontados: " + patternsFound);
+    }
+
+    private static boolean stillNeedsToComparePattern(
+        String stringToProcess, int stringIndex, String pattern
+    ) {
+        int stringLength = stringToProcess.length();
+        int patternLength = pattern.length();
+
+        return stringIndex <= (stringLength - patternLength);
     }
 
     public static void KMP(
         String stringToProcess, String pattern
     ) {
         int prefixTable[] = getKMPPrefixTable(pattern);
-        int patternsFound = 0;
+        int patternsFound = 0, comparsionsQuantity = 1;
 
         for(
             int stringIndex=0, patternIndex=0 ;
-            stringIndex<stringToProcess.length() ;
-            stringIndex++
+            stillNeedsToComparePattern(stringToProcess, stringIndex, pattern) ;
+            stringIndex++, comparsionsQuantity++
         ) {
-            char stringCharacter = stringToProcess.charAt(
-                stringIndex
-            );
-            char patternCharacter = pattern.charAt(
-                patternIndex
-            );
+            char stringChar = stringToProcess.charAt(stringIndex);
+            char patternChar = pattern.charAt(patternIndex);
 
-            boolean charactersAreDifferent = stringCharacter != patternCharacter;
-            if(charactersAreDifferent) {
+            if(stringChar != patternChar) {
                 if(patternIndex > 0) {
                     patternIndex = prefixTable[patternIndex - 1];
                     stringIndex--;
@@ -32,13 +66,23 @@ public class ProcessingProvider {
                 continue;
             }
 
-            if(patternIndex == pattern.length() - 1) {
-                patternIndex = prefixTable[patternIndex];
-                patternsFound++;
-            } else patternIndex++;
+            if(patternIndex != pattern.length() - 1) {
+                patternIndex++;
+                continue;
+            }
+            
+            patternIndex = prefixTable[patternIndex];
+            patternsFound++;
+
+            System.out.println(
+                "Foram feitas " + comparsionsQuantity +
+                " comparações para encontrar o " + patternsFound +
+                "º padrão."
+            );
+            comparsionsQuantity = 0;
         }
 
-        System.out.println(patternsFound);
+        System.out.println("Quantidade de padrões encontados: " + patternsFound);
     }
 
     private static int[] getKMPPrefixTable(String pattern) {
